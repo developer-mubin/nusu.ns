60707d9a4792be822fe92a5bc2292d7ac30248b8
Muntasir Mubin <mubin1@email.com>
2024-12-08 23:30:31 +0600

    Update parser.py

--- parser.py
+++ parser.py
@@ -4,13 +4,18 @@ def parse(tokens):
 
     for token in tokens:
         key, value = token
-        if key == "title":
+        if key == "@title:":
             html += f"    <title>{value}</title>\n"
-        elif key == "header":
+        elif key == "@head:":
             body += f"    <h1>{value}</h1>\n"
-        elif key == "paragraph":
+        elif key == "@pr:":
             body += f"    <p>{value}</p>\n"
-        elif key == "button":
+        elif key == "@link:"
+            body += f"    <a href="{value}</a>\n"
+        elif key == "@pic:":
+            body += f"    <img src="{value}">\n"
+        
+        elif key == "@btn:":
             text, link = value.split('->')
             body += f"    <button onclick=\"location.href='{link.strip()}'\">{text.strip()}</button>\n"
 
